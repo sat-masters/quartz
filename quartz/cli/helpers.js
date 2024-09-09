a9c0f4c439c9ee204c9cb2257688364b0bedd788
Albert Laursen <alaur20@student.aau.dk>
2024-09-09 14:05:16 +0200

    Quartz sync: Sep 9, 2024, 2:05 PM

--- quartz/cli/helpers.js
+++ quartz/cli/helpers.js
@@ -33,7 +33,7 @@ export async function stashContentFolder(contentFolder) {
 }
 
 export function gitPull(origin, branch) {
-  const flags = ["--no-rebase", "--autostash", "-s", "recursive", "-X", "ours", "--no-edit"]
+  const flags = ["--no-rebase", "--autostash", "-s", "recursive", "-X", "ours", "--no-edit", "--recurse-submodules"]
   const out = spawnSync("git", ["pull", ...flags, origin, branch], { stdio: "inherit" })
   if (out.stderr) {
     throw new Error(chalk.red(`Error while pulling updates: ${out.stderr}`))
